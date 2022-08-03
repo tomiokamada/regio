fa3c7d55185f62e7572f0b59b83f502ac55806f1
Tomio Kamada <kamada@fine.cs.kobe-u.ac.jp>
2022-08-03 19:51:02 +0900

    refactoring python programs

--- maxRange/dp.py
+++ maxRange/dp.py
@@ -1,19 +1,19 @@
-import sys
-input = sys.stdin.readline
-## debugの時はファイルから読み込む方が楽
-#input = open('maxRange/input/input1.txt', 'r').readline
+# debugの時はファイルから読み込む方が楽
+# 環境によっては、 `maxRange/input/input1.txt` などに変更しましょう
+# input = open('input/input1.txt', 'r').readline
 
 
 def main():
     n = int(input())
-    list = [int(input()) for i in range(n)]
-    maxS = 0
-    Si = 0
-    for li in list:
-        prevSi = Si
-        s = prevSi + li
-        Si = s if s>0 else 0
-        maxS = max(maxS, Si)
-    print(maxS)
+    numbers = [int(input()) for _ in range(n)]
+    max_s = 0
+    si = 0
+    for li in numbers:
+        prev_si = si
+        s = prev_si + li
+        si = s if s > 0 else 0
+        max_s = max(max_s, si)
+    print(max_s)
+
 
 main()
--- maxRange/tripleLoop.py
+++ maxRange/tripleLoop.py
@@ -1,16 +1,20 @@
+# debugの時はファイルから読み込む方が楽
+# 環境によっては、 `maxRange/input/input1.txt` などに変更しましょう
+# input = open('input/input1.txt', 'r').readline
 
 
 def main():
     n = int(input())
-    list = [ int(input()) for i in range(n) ]
+    numbers = [int(input()) for _ in range(n)]
     result = 0
     for i in range(n):
-        for j in range(i+1, n+1):
-            #s = 0
-            #for k in range(i, j):
-            #    s += list[k]
-            s = sum(list[i:j])
+        for j in range(i + 1, n + 1):
+            # s = 0
+            # for k in range(i, j):
+            #    s += numbers[k]
+            s = sum(numbers[i:j])
             result = max(result, s)
     print(result)
+
 
 main()
