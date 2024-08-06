d20797a05f13c642dd5f865f8e278483eb8f166e
Tomio Kamada <t_kamada@konan-u.ac.jp>
2024-08-06 09:40:33 +0900

    add japan_sinks

---
+++ algo1/japan_sinks_0.py
@@ -0,0 +1,24 @@
+
+N = int(input())
+As = list(map(int, input().split()))
+
+# As = [0 1 2 1 3 2]
+# に対し　A_pairs =    [(0, 0), (1, 1), (2, 2), (3, 1), (4, 3), (5, 2)]
+A_pairs = [  (index, As[index]) for index in range(N) ]
+# A_pair = list(enumerate(As)) # でも同じこと
+
+def my_order(pair):
+    index, val = pair
+    return -val  # val の大きい順に sort したいので、+/- 逆に
+#def my_order(pair): # こちらでも同じこと
+#   val = pair[1]
+#   return -val
+
+A_pairs.sort(key=my_order)
+# key でソートの基準値を与えることで、
+# ソート後は　A_pairs = [(4, 3), (2, 2), (5, 2), (1, 1), (3, 1), (0, 0)]　
+# (val の大きい順) のようになる
+# A_pairs.sort(key=lambda pair: -pair[1]) #でも同じこと
+print(A_pairs)
+
+
---
+++ algo1/japan_sinks_1.py
@@ -0,0 +1,28 @@
+
+N = int(input())
+As = list(map(int, input().split()))
+A_pair = list(enumerate(As))
+
+A_pair.sort(key=lambda x: -x[1])
+
+h_prev = 1000000001
+num_islands = 0
+islands_max = 0
+land=[False]*(N+2)
+
+for i, h in A_pair:
+    if h == 0:  # 海のところはパスして、最後で処理
+        continue
+    if h_prev != h: # 海抜が変わったときの処理
+        islands_max = max(num_islands, islands_max)
+        h_prev = h
+
+    land[i+1] = True
+    if land[i] and land[i+2]: #　島がつながる
+        num_islands -= 1
+    elif (not land[i]) and (not land[i+2]): # 独立した島が誕生
+        num_islands += 1
+    # else は、一方のみ島のケースは、島が広がるだけ
+
+islands_max = max(num_islands, islands_max)
+print(islands_max)
